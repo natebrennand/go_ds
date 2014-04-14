c7407a9f45cda5a73fb7916e6c0508e26c25532e
Nate Brennand <natebrennand@gmail.com>
2014-04-14 01:36:41 -0400

    try w/ byte arrays

--- bloom/array.go
+++ bloom/array.go
@@ -5,14 +5,15 @@ import (
 )
 
 type BitArray interface {
-	Get() bool
-	Set()
+	Get(uint64) bool
+	Set(uint64)
 }
 
 type BigIntBitArray []*big.Int
 
-func NewBigIntBitArray (size int) BigIntBitArray{
-	arr := make(BigIntBitArray, size)
+func NewBigIntBitArray(size uint64) BigIntBitArray {
+	numBits := int(1 + 10*size/64) // 10 bits/element
+	arr := make(BigIntBitArray, numBits)
 	for index := range arr {
 		arr[index] = big.NewInt(0)
 	}
@@ -21,7 +22,7 @@ func NewBigIntBitArray (size int) BigIntBitArray{
 
 func (b BigIntBitArray) Get(index uint64) bool {
 	intNum := int(index / uint64(len(b))) % len(b)
-	offset := int(index % uint64(len(b)))
+	offset := int(index % 64)
 	if 1 == b[intNum].Bit(offset) {
 		return true
 	}
@@ -30,6 +31,29 @@ func (b BigIntBitArray) Get(index uint64) bool {
 
 func (b BigIntBitArray) Set(index uint64) {
 	intNum := int(index / uint64(len(b))) % len(b)
-	offset := int(index % uint64(len(b))) % 2^64
+	offset := int(index % 64)
 	b[intNum].SetBit(b[intNum], offset, 1)
+}
+
+type ByteArray []byte
+
+func NewByteArray(size uint64) ByteArray {
+	numBits := int(1 + 10*size/8) // 10 bits/element
+	arr := make(ByteArray, numBits)
+	return arr
+}
+
+func (b ByteArray) Get(index uint64) bool{
+	byteNum := int(index / uint64(len(b))) % len(b)
+	offset := int(index % 8)
+	if b[byteNum] == b[byteNum] | byte(2^offset) {
+		return true
+	}
+	return false
+}
+
+func (b ByteArray) Set(index uint64) {
+	byteNum := int(index / uint64(len(b))) % len(b)
+	offset := int(index % 8)
+	b[byteNum] |= byte(2^offset)
 }
--- bloom/bloom.go
+++ bloom/bloom.go
@@ -1,22 +1,50 @@
 package bloom
 
-import (
-	"hash/fnv"
-)
-
 type BloomFilter struct {
-	bits BigIntBitArray
+	bits BitArray
+	hashes int
 }
 
-func NewBloomFilter(size uint64) BloomFilter {
-	return BloomFilter{NewBigIntBitArray(int(1 + 10*size/2^32))}
+func NewBloomFilter(size uint64, hashes int) BloomFilter {
+	// x := BloomFilter{NewBigIntBitArray(size), hashes}
+	x := BloomFilter{NewByteArray(size), hashes}
+	return x
 }
 
-func (b BloomFilter) Add (elem interface{}) {
-	b.bits.Set(fnv.New64().Sum64())
-	b.bits.Set(fnv.New64a().Sum64())
+func (b BloomFilter) Add(elem interface{}) {
+	hash1, err := ComputeHash(elem)
+	if err != nil {
+		panic("Hash fn failed")
+	}
+	b.bits.Set(hash1)
+
+	hashN := hash1
+	for i := 1; i < b.hashes; i++ {
+		hashN, err := ComputeHash(hashN)
+		if err != nil {
+			panic("Hash fn failed")
+		}
+		b.bits.Set(hashN)
+	}
 }
 
-func (b BloomFilter) Contains (elem interface{}) bool {
-	return b.bits.Get(fnv.New64().Sum64()) && b.bits.Get(fnv.New64a().Sum64())
+func (b BloomFilter) Contains(elem interface{}) bool {
+	hash1, err := ComputeHash(elem)
+	if err != nil {
+		panic("Hash fn failed")
+	}
+	if !b.bits.Get(hash1) {
+		return false
+	}
+
+	for i := 1; i < b.hashes; i++ {
+		hashN, err := ComputeHash(elem)
+		if err != nil {
+			panic("Hash fn failed")
+		}
+		if !b.bits.Get(hashN) {
+			return false
+		}
+	}
+	return true
 }
